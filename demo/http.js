b30cc8f6de3df0ee9b06ef9f6d73ad6a62b4b3bb
yehua <33891401+yehuachun@users.noreply.github.com>
2023-02-21 16:43:32 +0800

    test

--- demo/http.js
+++ demo/http.js
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static')
 var staticBasePath = './demo';
  
 var app = express()
- 
+// fasdf
 app.use(serveStatic(staticBasePath))
 app.listen(8080)
 console.log('Listening on port 8080');
